c3dea8b9c384038366819b968d250a3f3e481ef7
Pascal Bell <pascal.bell@duke.edu>
2023-07-24 18:20:59 -0400

    created node mailer

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import nodemailer from "nodemailer";
 dotenv.config();
 
 if(!process.env.API_KEY){
@@ -22,3 +23,30 @@ const baseUrl = "https://secure.tutorcruncher.com/api/";
  */
 export const apiUrl = (path: string) => baseUrl + path.padStart(1, "/").substring(1);
 
+export const mailTransporter = () => {                      // where emails will be sent from
+    const transporter = nodemailer.createTransport({
+        service: "gmail",                                 // service provider for this email
+        auth: {
+            user: process.env.EMAIL_ADDRESS,               // username and password for the email
+            pass: process.env.EMAIL_PASSWORD
+        }
+    });
+    return transporter;
+};
+
+export const sendVerification = (user: any) => {        // sends verification email
+    const transporter = mailTransporter();
+
+    const mailOptions = {
+        from: '"FFSD Email Verification" <`process.env.EMAIL_ADDRESS`>',    // change title and email here
+        to: "pascalbell16@gmail.com",
+        subject: "",                                // change subject here
+        html: `<h>Hello ${user.first_name}</h>
+            <p> Thank you for registering! Please click below to verify your email adress and proceed to payment:</p>`
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) return console.log(error);
+        console.log("verification sent");
+    });
+}
