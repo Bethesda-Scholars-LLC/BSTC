c7f17714b67895ff2538f479710bf0a4d49529fb
Pascal Bell <pascal.bell@duke.edu>
2023-08-16 12:00:42 +0530

    finalized referral email

--- src/mail/tutorReferral.tsx
+++ src/mail/tutorReferral.tsx
@@ -1,7 +1,7 @@
 import { MailOptions } from "nodemailer/lib/sendmail-transport";
 import { ContractorObject } from "../integration/tc models/contractor/types";
 import { BUSINESS_EMAIL_FROM, PROD } from "../util";
-import React from "react";
+import React, { CSSProperties } from "react";
 import { getUserFirstName } from "../integration/tc models/user/user";
 import ReactDOMServer from "react-dom/server";
 
@@ -16,10 +16,45 @@ export const tutorReferralMail = (contractor: ContractorObject): MailOptions => 
 };
 
 const ReferralEmail = (props: {contractor: ContractorObject}) => {
-    return <p>
+    const mediumIndent: CSSProperties = {
+        paddingLeft: "20px"
+    };
+    return <p style={{margin: 0}}>
         Hi {getUserFirstName(props.contractor.user)},
         <br/>
-        Bethesda scholars has a referral program. You are the goat that has been chosen for it. you will grow our business
-        20x in 1 year because thats how good you are. now give everyone this code: <b>{props.contractor.id}</b>
+        <br/>
+        Welcome to Bethesda Scholars' Referral Program. Tell your friends to sign up as a tutor!
+        <br/>
+        <br/>
+        <b>How it works:</b>
+        <ol style={mediumIndent}>
+            <li>
+                Your referral code is <b style={{backgroundColor: "yellow"}}>{props.contractor.id}</b>. Give this to a friend to sign up with.
+            </li>
+            <li>
+                Have your friend put your referral code in their profile under the "Referral Code" field.
+            </li>
+            <li>
+                We will add <b>$10</b> to your monthly pay--see criteria below.
+            </li>
+        </ol>
+        <b>To recieve your referral payment, the tutor you refer us must:</b>
+        <ol style={mediumIndent}>
+            <li>
+                Pass our screening process.
+            </li>
+            <li>
+                Be a high school senior.
+            </li>
+            <li>
+                Attend either Walt Whitman HS or Bethesda Chevy Chase HS in Bethesda, MD.
+            </li>
+        </ol>
+        If you have any questions, feel free to reply directly to this email.
+        <br/>
+        <br/>
+        Thanks,
+        <br/>
+        {process.env.BUSINESS_EMAIL_FROM}
     </p>;
 };
