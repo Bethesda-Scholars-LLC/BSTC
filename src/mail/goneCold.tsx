2aa1267a26c4cebcb5196b61cc8f19613a53e232
Pascal Bell <pascal.bell@duke.edu>
2023-08-24 20:21:22 -0400

    added signature to cold email

--- src/mail/goneCold.tsx
+++ src/mail/goneCold.tsx
@@ -34,5 +34,15 @@ const GoneCold = (props: {job: JobObject, client: ClientObject | null, contracto
         <br/>
         <br/>
         Thanks,
+        <br/>
+        <b>{process.env.PERSONAL_EMAIL_FROM}</b>
+        <br/>
+        {process.env.SIGNATURE_DESCRIPTION}
+        <br/>
+        _________________________________
+        <br/>
+        <b>Website</b>: https://www.bethesdascholars.com
+        <br/>
+        <b>Mobile</b>: 202-294-6538
     </p>;
 };
