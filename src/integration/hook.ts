fd08a7767c364dfd9a59f6dda99ed71aea130e1d
cchosch <colinhoscheit@gmail.com>
2023-12-12 19:36:04 -0500

    send invalid request response if its signature

--- src/integration/hook.ts
+++ src/integration/hook.ts
@@ -29,7 +29,7 @@ hookRouter.all("*", (req: Req, res: Res) => {
 
         if(verifyHook !== req.headers["webhook-signature"]){
             Log.debug(`invalid request ${JSON.stringify(req.body, undefined, 2)}`);
-            return res.status(400).send();
+            return res.json({error: "invalid request"}).status(400).send();
         }
 
         Log.debug(req.body);
