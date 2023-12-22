b4492997aecd0e66bf7c9b74f7d87a95e0d4c582
pascalbell <pascalbell16@gmail.com>
2023-12-22 19:32:26 +1100

    disabled cold job for winter break

--- src/integration/tc models/service/service.ts
+++ src/integration/tc models/service/service.ts
@@ -371,10 +371,11 @@ addTCListener("CHANGED_SERVICE_STATUS", async (event: TCEvent<any, any>) => {
         if (notCold) {
 
             // COMMENT AFTER THANKSGIVING AND CHRISTMAS
+            /*
             transporter.sendMail(goneColdMail(job, client, contractor), (err) => {
                 if (err)
                     Log.error(err);
-            });
+            });*/
             // await NotCold.findByIdAndDelete(notCold.id);
             
             
