79a112b0a8c76bbac33b9f6336dcae8569de28c3
pascalbell <pascalbell16@gmail.com>
2025-03-21 11:02:12 -0400

    testing if req.body hmac works

--- src/integration/hook.ts
+++ src/integration/hook.ts
@@ -31,13 +31,13 @@ export const addTCListener = (eventNames: string | string[], listener: TCEventLi
 hookRouter.all("*", async (req: Req, res: Res) => {
     if(req.body?.events && req.rawBody){
         const verifyHook = createHmac("sha256", process.env.API_KEY!) // eslint-disable-line
-            .update(req.rawBody)
+            .update(req.body)
             .digest("hex");
 
-        // if(verifyHook !== req.headers["webhook-signature"]){
-        //     Log.debug(`invalid request ${JSON.stringify(req.body, undefined, 2)}`);
-        //     return res.status(400).json({error: `invalid request ${verifyHook} ${req.headers["webhook-signature"]}`}).send();
-        // }
+        if(verifyHook !== req.headers["webhook-signature"]){
+            Log.debug(`invalid request ${JSON.stringify(req.body, undefined, 2)}`);
+            return res.status(400).json({error: `invalid request ${verifyHook} ${req.headers["webhook-signature"]}`}).send();
+        }
         
         const events: TCEvent[] = req.body.events;
         for(let i = 0; i < events.length; i++){
