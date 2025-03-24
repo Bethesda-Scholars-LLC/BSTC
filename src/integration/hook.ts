97a68f1b5a74c5f4a59e98660aa55d2aa2100be8
pascalbell <pascalbell16@gmail.com>
2025-03-24 13:53:27 -0400

    webhook verification fixed on TC

--- src/integration/hook.ts
+++ src/integration/hook.ts
@@ -34,13 +34,10 @@ hookRouter.all("*", async (req: Req, res: Res) => {
             .update(req.rawBody)
             .digest("hex");
         
-        Log.info("RAW BODY: ", req.rawBody);
-        Log.info("Parsed body: ", JSON.stringify(req.body));
-        Log.info("hmac generated from raw: ", verifyHook);
-        // if(verifyHook !== req.headers["webhook-signature"]){
-        //     Log.error(`invalid request ${JSON.stringify(req.body, undefined, 2)}`);
-        //     return res.status(400).json({error: `invalid request ${verifyHook} ${req.headers["webhook-signature"]}`}).send();
-        // }
+        if(verifyHook !== req.headers["webhook-signature"]){
+            Log.error(`invalid request ${JSON.stringify(req.body, undefined, 2)}`);
+            return res.status(400).json({error: `invalid request ${verifyHook} ${req.headers["webhook-signature"]}`}).send();
+        }
 
         const events: TCEvent[] = req.body.events;
         for(let i = 0; i < events.length; i++){
